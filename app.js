423e22e6aa9753fe3fcc0ace683aaa47b188714b
Erik M Baxstrom <baxstrem@gmail.com>
2022-09-28 16:21:16 -0700

    add ability to delete all todos

--- app.js
+++ app.js
@@ -2,7 +2,12 @@
 // this will check if we have a user and set signout link if it exists
 import './auth/user.js';
 // Part A: import create todo
-import { createTodo, getTodos, completeTodo } from './fetch-utils.js';
+import {
+    createTodo,
+    getTodos,
+    completeTodo,
+    deleteAllTodos,
+} from './fetch-utils.js';
 // Part B: import get todos
 // Part C: import complete todos
 // Part D: import delete all function
@@ -56,14 +61,14 @@ addTodoForm.addEventListener('submit', async (e) => {
 
 removeButton.addEventListener('click', async () => {
     // > Part D: Call the async supabase function to delete all todos
-    const response = null; // change me
+    const response = deleteAllTodos(); // change me
     error = response.error;
 
     if (error) {
         displayError();
     } else {
         // > Part D: reset todos state to an empty array:
-
+        todos = [];
         displayTodos();
     }
 });
